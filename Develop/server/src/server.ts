021ff86cb6401eec2242ab3b26cb94714246537b
gabipayan <gapayan26@gmail.com>
2024-08-22 22:26:02 -0600

    Update API key in .env.example and serve static files from client/dist folder

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -2,19 +2,34 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 
+// Construct __dirname since it's not available in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Import the routes
 import routes from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
+
+
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Implement middleware to log requests
+app.use((req, _res, next) => {
+  console.log(`Request: ${req.method} ${req.path}`);
+  next();
+});
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
